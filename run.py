4d64f52d257673cdb77e8f4d53e9f1f590145da9
t0tacci0 <>
2024-02-04 03:41:15 +0000

    Add welcome messages

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import colorama
 from colorama import Fore, Style, init
 
 SCOPE = [
@@ -13,4 +14,9 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('code_survey')
 
-print("Welcome to the Code Survey Program!")
+print(f"{Fore.GREEN}Welcome to the Code Survey Program!")
+print(f"{Fore.GREEN}This survey is made to help people to have a better vision of the world of coding")
+print(f"{Fore.GREEN}and at the same time to get information from answers.")
+print(Style.RESET_ALL)
+print(f"{Fore.YELLOW}Please enter your name...")
+print(f"{Fore.YELLOW}Name example: MyName")
