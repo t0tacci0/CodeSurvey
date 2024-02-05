1266d7aef64b639d66caab9f1a0d008775c74ad0
t0tacci0 <>
2024-02-05 02:43:03 +0000

    Update welcome function

--- run.py
+++ run.py
@@ -18,20 +18,25 @@ SHEET = GSPREAD_CLIENT.open('code_survey')
 
 def welcome():
     print(f"{Fore.MAGENTA}{Style.BRIGHT}Welcome to the Code Survey Program!\n")
-    time.sleep(4)
+    time.sleep(5)
     print(f"{Fore.MAGENTA}This survey is made to help people to have a better vision of the world of coding")
     print(f"{Fore.MAGENTA}and at the same time to get information from answers.\n")
+    time.sleep(6)
+    print(f"{Fore.MAGENTA}Soon you will be asked a series of questions,")
+    print(f"{Fore.MAGENTA}Please answer truthfully and enjoy!\n")
     time.sleep(5)
 
     while True:
         user_name = input(f"{Fore.YELLOW}Please enter your name: ")
+        print()
         if re.match("^[a-zA-Z ]+$", user_name):
             break
         else:
             print(f"{Fore.RED}Invalid name. Please enter a valid name containing only letters and spaces.")
 
-    print(f"{Fore.YELLOW}Hello, {user_name}! Thank you for participating in the Code Survey.")
-    time.sleep(1)
+    print(f"{Fore.YELLOW}Hello, {user_name}! Thank you for participating in the Code Survey.\n")
+    time.sleep(3)
+    print(f"{Fore.YELLOW}Get Ready!")
     print(Style.RESET_ALL)
 
 
